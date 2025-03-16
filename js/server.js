bf915d2701ca587380fd7818b3e80dcca02e3587
Veries Seals <veries@me.com>
2025-03-16 12:30:53 -0500

    Adding node modules

--- js/server.js
+++ js/server.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
-// Create Varible to create mySQL database
+// Create Varible to connect to the mySQL database
 // -------------------------------------------------
 
 const db = mysql.createConnection({
@@ -17,4 +17,40 @@ const db = mysql.createConnection({
     password: '',
     database: 'school'
 });
+
+// If an error occurs while to connecting to the mySQL database or successful connection
+// -------------------------------------------------
+
+db.connect(err => {
+    if (err) throw err;
+    console.log('MySQL Connected...');
+});
+
+// App Post to mySQL database
+// -------------------------------------------------
+
+app.post('/api/register', (req, res) => {
+    const { firstName, lastName, email, grade, school } = req.body;
+    const sql = 'INSERT INTO students (first_name, last_name, email, grade, school) VALUES (?, ?, ?, ?, ?)';
+    db.query(sql, [firstName, lastName, email, grade, school], (err, result) => {
+        if (err) throw err;
+        res.json({message: 'Registation successful!' });
+    });
+});
+
+// App Get information from Database
+// -------------------------------------------------
+
+app.get('/api/students', (req, res) => {
+    const sql = 'SELECT * FROM students';
+    db.query(sql, (err, results) => {
+        if (err) throw err;
+        res.json(results);
+        })
+});
+
+app.listen(3000, () => console.log("Server is on and poppin!"));
+
+
+
 
